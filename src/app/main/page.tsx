01d2a5fbc7bb5ecb017ab052b57a3a9330f0ed78
rlawo <rlawo32@naver.com>
2026-01-12 00:39:22 +0900

    [MovieFilter][FE] Main_Page_작업_20260112 (1)

---
+++ src/app/main/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+import * as Style from "./page.style";
+
+const Main = () => {
+
+    return (
+        <Style.Main>
+            <h1>MAIN PAGE</h1>
+        </Style.Main>
+    )
+}
+
+export default Main;
