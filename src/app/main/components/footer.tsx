827561d4104d43de004955c35bcbc71adb3e0b87
rlawo <rlawo32@naver.com>
2026-01-14 00:07:34 +0900

    [MovieFilter][FE] Header&Footer_추가_20260114

---
+++ src/app/main/components/footer.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import styled from "styled-components";
+
+const FooterStyle = styled('div')`
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    width: 100%;
+    height: 50px;
+    background-color: #0F0F0F;
+    color: white;
+`
+
+const Footer = () => {
+
+    return (
+        <FooterStyle>
+            
+        </FooterStyle>
+    )
+}
+
+export default Footer;
