d70ae8ef7ee6a01848bf1db6ccf78a1717c72305
rlawo <rlawo32@naver.com>
2026-01-15 20:06:32 +0900

    [MovieFilter][FE] Login_버튼_이동_작업_20260115

--- src/app/main/components/header.tsx
+++ src/app/main/components/header.tsx
@@ -2,6 +2,8 @@
 
 import styled from "styled-components";
 
+import { useRouter } from 'next/navigation'
+
 const HeaderStyle = styled('div')`
     position: fixed;
     top: 0;
@@ -14,15 +16,17 @@ const HeaderStyle = styled('div')`
     padding: 0 50px;
     background-color: #0F0F0F;
     color: white;
+    z-index: 1000;
 `
 
 
 const Header = () => {
+    const router = useRouter();
 
     return (
         <HeaderStyle>
             <div>LOGO</div>
-            <div>LOGIN</div>
+            <button onClick={() => router.push('/login')}>LOGIN</button>
         </HeaderStyle>
     )
 }
