e38bd5e35dd89d79edc0ef9761c9ee5c3187e4cb
rlawo <rlawo32@naver.com>
2026-01-15 20:40:22 +0900

    [MovieFilter][FE] Main_Option_프로세스_작업_20260115 (1)

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -2,12 +2,18 @@
 
 import * as Style from "./page.style";
 
+import useMainProcessStore from "../stores/useMainProcessStore";
+
+import Option from "./components/option";
+
 const Main = () => {
+
+    const {process, setProcess} = useMainProcessStore();
 
     return (
         <Style.Main>
             <div className="main_start">
-                <button>시작하기</button>
+                {process > 0 ? <Option /> : <button className="main_button" onClick={() => setProcess(1)}>시작하기</button>}
             </div>
         </Style.Main>
     )
