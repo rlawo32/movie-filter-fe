f5a9884ca332677dcadb846e794567870743110d
rlawo <rlawo32@naver.com>
2026-01-21 19:34:13 +0900

    [MovieFilter][FE] FontSize_전역_변경_20260121

--- src/app/main/page.tsx
+++ src/app/main/page.tsx
@@ -13,7 +13,8 @@ const Main = () => {
     return (
         <Style.Main>
             <div className="main_start">
-                {process > 0 ? <Option /> : <button className="main_button" onClick={() => setProcess(1)}>시작하기</button>}
+                <Option />
+                {process > 0 ? <></> : <button className="main_button" onClick={() => setProcess(1)}>시작하기</button>}
             </div>
         </Style.Main>
     )
