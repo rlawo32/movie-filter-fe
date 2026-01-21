ff7d88ca8baa61ec5cc1ef1870825e849bf43ded
rlawo <rlawo32@naver.com>
2026-01-21 19:34:42 +0900

    [MovieFilter][FE] Main_Option_프로세스_작업_20260121 (2)

--- src/app/main/components/option.style.tsx
+++ src/app/main/components/option.style.tsx
@@ -1,9 +1,9 @@
-import styled, { keyframes } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
 const fadeIn = keyframes`
   from {
     opacity: 0;
-    transform: translateY(10px); /* 살짝 아래에서 위로 올라오는 효과 */
+    transform: translateY(10px); 
   }
   to {
     opacity: 1;
@@ -11,7 +11,16 @@ const fadeIn = keyframes`
   }
 `;
 
-export const OverlayStyle = styled('div')`
+const rotate = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+export const OverlayStyle = styled('div')<{$process:number}>`
     position: fixed;
     top: 0;
     left: 0;
@@ -22,7 +31,7 @@ export const OverlayStyle = styled('div')`
     height: 100%;
     background: rgba(0, 0, 0, 0.1);
     backdrop-filter: blur(0.5px); 
-    z-index: 1000;
+    z-index: ${({$process}) => $process > 0 ? 1000 : -1};
 `
 
 export const OptionStyle = styled('div')<{$process:number}>`
@@ -31,15 +40,58 @@ export const OptionStyle = styled('div')<{$process:number}>`
     flex-direction: column;
     justify-content: center;
     width: 470px;
-    height: 350px;
-    padding: 40px;
-    border: 1px solid rgba(255, 255, 255, 0.1); /* 미세한 테두리 */
+    height: 430px;
+    padding: 30px;
+    border: 1px solid rgba(255, 255, 255, 0.1);
     border-radius: 16px;
     background: rgba(30, 30, 30, 0.9);
     box-shadow: 0 20px 40px rgba(0, 0, 0, 0.4);
     color: #e0e0e0;
     text-align: center;
     z-index: 9999;
+    opacity: ${({$process}) => $process > 0 ? 1 : 0};
+    transition: opacity .5s ease-in-out;
+
+    .modal_close {
+        position: absolute;
+        top: 7px;
+        right: 7px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 20px;
+        height: 20px;
+        border: 1px solid rgba(172, 229, 255, 0.2);
+        border-radius: 8px;
+        background: rgba(172, 229, 255, 0.1);
+        font-size: 1rem;
+        color: #ACE5FF;
+        cursor: pointer;
+        transition: all 0.3s ease;
+
+        &:hover {
+            background: rgba(172, 229, 255, 0.2);
+            border-color: #ACE5FF;
+            box-shadow: 0 0 10px rgba(172, 229, 255, 0.3);
+        }
+    }
+
+    .option_progress {
+        position: absolute;
+        top: 35px;
+        left: 0;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+
+        .progress_item {
+
+            .item_title {
+
+            }
+        }
+    }
 
     .option_view {
         display: flex;
@@ -47,52 +99,29 @@ export const OptionStyle = styled('div')<{$process:number}>`
         justify-content: center;
         align-items: center;
         align-content: center;
-        padding-bottom: 70px;
+        padding-bottom: 20px;
 
         .option_item {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            width: ${({$process}) => $process === 1 ? 120 : 105}px;
-            height: ${({$process}) => $process === 1 ? 70 : 50}px;
-            padding: 10px;
-            margin: 10px;
-            border: 1px solid transparent;
-            border-radius: 8px;
-            background: rgba(255, 255, 255, 0.05);
-            font-size: 1.1rem;
-            cursor: pointer;
-            transition: all 0.3s ease;
-            animation: ${fadeIn} 1s ease-out forwards;
-
-            .option_icon {
-                padding-top: 2px;
-                margin-right: 5px;
-            }
-            
-            &:hover {
-                background: rgba(255, 255, 255, 0.1);
-                border: 1px solid #D4AF37;
-            }
         }
     }
 
     .option_next {
         position: absolute;
-        bottom: 30px;
+        bottom: 35px;
         left: 0;
         width: 100%;
     
         button {
-            width: 50%;
-            background: rgba(0, 150, 255, 0.15); 
-            border: 1px solid rgba(135, 206, 250, 0.5);
-            color: #ACE5FF; 
+            width: ${({$process}) => $process > 1 ? 30 : 50}%;
             padding: 12px 28px;
+            margin: 0 15px;
+            border: 1px solid rgba(135, 206, 250, 0.5);
             border-radius: 8px;
+            color: #ACE5FF; 
+            background: rgba(0, 150, 255, 0.15); 
             backdrop-filter: blur(4px);
             box-shadow: 0 0 15px rgba(0, 150, 255, 0.1);
-            transition: all 0.3s ease;
+            transition: background box-shadow color 0.3s ease;
             cursor: pointer;
 
             &:hover {
@@ -102,4 +131,106 @@ export const OptionStyle = styled('div')<{$process:number}>`
             }
         }
     }
+`
+
+export const OptionItem = styled('div')<{$process:number, $select:boolean}>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: ${({$process}) => $process === 1 ? 120 : 105}px;
+    height: ${({$process}) => $process === 1 ? 70 : 50}px;
+    padding: 10px;
+    margin: 10px;
+    border: 1px solid ${({$select}) => $select ? '#ACE5FF' : 'transparent'};
+    border-radius: 8px;
+    background: rgba(255, 255, 255, 0.05);
+    cursor: pointer;
+    opacity: 0;
+    transition: all 0.3s ease;
+    animation: ${fadeIn} 1s ease-out .2s forwards;
+
+    .option_icon {
+        padding-bottom: 2px;
+        font-size: 1.7rem;
+    }
+
+    .option_title {
+        font-size: 1.5rem;
+    }
+    
+    &:hover {
+        background: rgba(255, 255, 255, 0.1);
+        border: 1px solid #ACE5FF;
+    }
+`
+
+export const ProgressBar = styled('div')`
+    position: relative;
+    width: 120px;
+    height: 2px;
+    margin-bottom: 20px;
+    background-color: #062033;
+`
+
+export const ProgressGauge = styled('div')<{$process:boolean}>`
+    width: ${({$process}) => $process ? 120 : 0}px;
+    height: 2px;
+    background-color: #ACE5FF;
+    transition: width 1s ease;
+`
+
+export const ProgressItem = styled('div')<{$process:boolean}>`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.1rem;
+
+    .item_icon {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 25px;
+        height: 25px;
+        margin-bottom: 5px;
+        border: 1px solid rgba(135, 206, 250, 0.5);
+        border-radius: 50%;
+        background: rgba(0, 150, 255, 0.15); 
+        backdrop-filter: blur(4px);
+        box-shadow: 0 0 15px rgba(0, 150, 255, 0.1);
+        overflow: hidden;
+
+        ${({ $process }) => $process && css`
+            &::before {
+                content: '';
+                position: absolute;
+                width: 101%;
+                height: 101%;
+                background: conic-gradient(
+                    transparent, 
+                    rgba(135, 206, 250, 0.8), 
+                    transparent 30%
+                );
+                animation: ${rotate} 2s linear infinite;
+            }
+        `}
+
+        &::after {
+            content: '';
+            position: absolute;
+            inset: 1px;
+            background: rgba(0, 10, 30, .9);
+            border-radius: 50%;
+            z-index: 1;
+        }
+
+        .progress_icon {
+            padding: 0;
+            font-size: 1.3rem;
+            color: #ACE5FF; 
+            z-index: 2;
+        }
+    }
+
 `
--- src/app/main/components/option.tsx
+++ src/app/main/components/option.tsx
@@ -4,89 +4,156 @@ import * as Style from "./option.style";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faSmile as smile
+    faChild as personnel_solo,
+    faUserGroup as personnel_freind,
+    faChildren as personnel_couple,
+    faUsers as personnel_family,
+    faCheck as check,
 } from "@fortawesome/free-solid-svg-icons";
+import {
+    faMeh as motion_normal,
+    faLaugh as motion_happy, 
+    faSadTear as motion_sad, 
+    faAngry as motion_angry,
+    faGrimace as motion_petulance,
+    faLaughSquint as motion_excited,
+    faDizzy as motion_unrest,
+    faFrown as motion_helpless,
+    faTired as motion_tired,
+} from "@fortawesome/free-regular-svg-icons";
 
 import { useEffect, useState } from "react";
 import useMainProcessStore from "../../stores/useMainProcessStore";
 
 import useSupabaseBrowser from "@/app/supabase/supabase-browser";
 import { useQuery } from "@supabase-cache-helpers/postgrest-react-query";
-import { 
-    getOptionAllQuery, getOptionPersonnelQuery, getOptionMotionQuery, getOptionGenreQuery 
-} from "@/app/queries/getOptionQuery";
+import { getOptionAllQuery } from "@/app/queries/getOptionQuery";
+import axios from "axios";
 
 const Option = () => {
     const supabase = useSupabaseBrowser();
 
-    const {process, setProcess} = useMainProcessStore();
+    const {process, setProcess, optionArr, setOptionArr, optionClean} = useMainProcessStore();
 
     const { data: optionAll } = useQuery(getOptionAllQuery(supabase), {staleTime: Infinity, gcTime: 1000 * 60 * 60});
-    const { data: optionPersonnel } = useQuery(getOptionPersonnelQuery(supabase), {enabled:process===1});
-    const { data: optionMotion } = useQuery(getOptionMotionQuery(supabase), {enabled:process===2});
-    const { data: optionGenre } = useQuery(getOptionGenreQuery(supabase), {enabled:process===3});
     const [optionData, setOptionData] = useState<{option_title:string}[]>([]);
 
+    const recommendMovieActive = () => {
+        axios({
+            method: "POST",
+            url: "/local/api/test",
+            data: JSON.stringify(optionArr),
+            headers: {'Content-type': 'application/json'}
+        }).then((res):void => {
+            console.log(res.data);
+        }).catch((err):void => {
+            alert("서버를 확인해주세요.");
+            console.log(err.message);
+        })
+    }
+
+    const closeModal = () => {
+        setProcess(0);
+        optionClean();
+    }
+
     useEffect(() => {
-        if(process > 1) setOptionData([]);
+        if(process > 0) setOptionData([]);
         setTimeout(() => {
             if(!!optionAll) {
                 if(process === 1) {
                     setOptionData(optionAll.filter((item) => item.option_type === 'P'));
-                    console.log("Personnel");
                 } else if(process === 2) {
                     setOptionData(optionAll.filter((item) => item.option_type === 'M'));
-                    console.log("Motion");
                 } else if(process === 3) {
                     setOptionData(optionAll.filter((item) => item.option_type === 'G'));
-                    console.log("Genre");
                 }
             }
         }, 100)
     }, [process, optionAll])
 
     return (
-        <Style.OverlayStyle> 
+        <Style.OverlayStyle $process={process}> 
             <Style.OptionStyle $process={process}>
+                <button className="modal_close" onClick={() => closeModal()}>X</button>
+                <div className="option_progress">
+                    <Style.ProgressItem $process={process === 1 ? true : false}>
+                        <div className="item_icon">
+                            {process > 1 ? <FontAwesomeIcon icon={check} className="progress_icon" /> : <></>}
+                        </div>
+                        <div className="item_title">
+                            인원
+                        </div>
+                    </Style.ProgressItem>
+                    <Style.ProgressBar>
+                        <Style.ProgressGauge $process={process > 1 ? true : false} />
+                    </Style.ProgressBar>
+                    <Style.ProgressItem $process={process === 2 ? true : false}>
+                        <div className="item_icon">
+                            {process > 2 ? <FontAwesomeIcon icon={check} className="progress_icon" /> : <></>}
+                        </div>
+                        <div className="item_title">
+                            감정
+                        </div>
+                    </Style.ProgressItem>
+                    <Style.ProgressBar>
+                        <Style.ProgressGauge $process={process > 2 ? true : false} />
+                    </Style.ProgressBar>
+                    <Style.ProgressItem $process={process === 3 ? true : false}>
+                        <div className="item_icon">
+                            {process > 3 ? <FontAwesomeIcon icon={check} className="progress_icon" /> : <></>}
+                        </div>
+                        <div className="item_title">
+                            장르
+                        </div>
+                    </Style.ProgressItem>
+                </div>
                 <div className="option_view">
                     {
                         optionData.map((item:any, idx) => {
                             return (
-                                <div className="option_item" key={idx}>
+                                <Style.OptionItem key={idx} $process={process} $select={optionArr.some(option => option.id === item.option_id)}
+                                                  onClick={() => setOptionArr(item.option_id, item.option_type, item.option_title)} >
                                     {
-                                        item.option_id === 'option_1' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_2' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_3' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_4' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_5' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_6' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_7' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_8' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_9' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_10' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_11' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_12' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_13' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_14' ? <FontAwesomeIcon icon={smile} className="option_icon" /> : 
-                                        item.option_id === 'option_15' ? <FontAwesomeIcon icon={smile} className="option_icon" /> : 
-                                        item.option_id === 'option_16' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_17' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_18' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_19' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_20' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_21' ? <FontAwesomeIcon icon={smile} className="option_icon" /> :
-                                        item.option_id === 'option_22' ? <FontAwesomeIcon icon={smile} className="option_icon" /> : <></>
+                                        item.option_id === 'option_1' ? <FontAwesomeIcon icon={personnel_solo} className="option_icon" /> :
+                                        item.option_id === 'option_2' ? <FontAwesomeIcon icon={personnel_freind} className="option_icon" /> :
+                                        item.option_id === 'option_3' ? <FontAwesomeIcon icon={personnel_couple} className="option_icon" /> :
+                                        item.option_id === 'option_4' ? <FontAwesomeIcon icon={personnel_family} className="option_icon" /> :
+                                        item.option_id === 'option_5' ? <FontAwesomeIcon icon={motion_normal} className="option_icon" /> :
+                                        item.option_id === 'option_6' ? <FontAwesomeIcon icon={motion_happy} className="option_icon" /> :
+                                        item.option_id === 'option_7' ? <FontAwesomeIcon icon={motion_sad} className="option_icon" /> :
+                                        item.option_id === 'option_8' ? <FontAwesomeIcon icon={motion_angry} className="option_icon" /> :
+                                        item.option_id === 'option_9' ? <FontAwesomeIcon icon={motion_petulance} className="option_icon" /> :
+                                        item.option_id === 'option_10' ? <FontAwesomeIcon icon={motion_excited} className="option_icon" /> :
+                                        item.option_id === 'option_11' ? <FontAwesomeIcon icon={motion_unrest} className="option_icon" /> :
+                                        item.option_id === 'option_12' ? <FontAwesomeIcon icon={motion_helpless} className="option_icon" /> :
+                                        item.option_id === 'option_13' ? <FontAwesomeIcon icon={motion_tired} className="option_icon" /> : <></>
                                     }
-                                    {item.option_title}
-                                </div>
+                                    <div className="option_title">{item.option_title}</div>
+                                </Style.OptionItem>
                             )  
                         })
                     }
                 </div>
                 <div className="option_next">
-                    <button onClick={() => setProcess(process+1)}>
-                        {process === 3 ? "완료" : "다음"}
-                    </button>
+                    {
+                        process > 1 ?
+                            <button onClick={() => setProcess(process-1)}>
+                                뒤로
+                            </button>
+                            :
+                            <></>
+                    }
+                    {
+                        process > 2 ? 
+                            <button onClick={() => recommendMovieActive()}>
+                                완료
+                            </button>
+                            : 
+                            <button onClick={() => setProcess(process+1)}>
+                                다음
+                            </button>
+                    }
                 </div>
             </Style.OptionStyle>
         </Style.OverlayStyle>
